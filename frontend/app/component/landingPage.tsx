c11ba77f89df425bacab965367dc0e98fd3bef5c
RupeshKhadka69 <khadkarupesh456@gmail.com>
2024-01-27 22:54:26 +0545

    initialized frontend

---
+++ frontend/app/component/landingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='text-center'>
+        <h3 className='text-xl '> This is the Home Page</h3>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import LandingPage from "./component/landingPage";
+export default function Home() {
+  return (
+        <>
+        <LandingPage/>
+        </>
+  );
+}
